2230a77d9f68e3d699a0d5df10d7848436c570ab
yoohunnji <yoohunn@awesome.dev>
2024-04-07 17:01:46 +0900

    feat: 페이지 기본 스타일 추가

--- app/accordion/page.tsx
+++ app/accordion/page.tsx
@@ -1,3 +1,10 @@
+import styles from "./styles.module.css";
+
 export default function AccordionPage() {
-  return <h1 className="h-screen">accordion</h1>;
+  return (
+    <main className="prose h-screen flex flex-col item-center p-20">
+      <h1>Accordion</h1>
+      <section>gkgk</section>
+    </main>
+  );
 }
