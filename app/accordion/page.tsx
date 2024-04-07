79e9f4b5e815e0dbf0743b984065cc0e19edf897
yoohunnji <yoohunn@awesome.dev>
2024-04-07 17:31:15 +0900

    feat: 페이지 코드 추가

--- app/accordion/page.tsx
+++ app/accordion/page.tsx
@@ -1,10 +1,21 @@
+import { CodeBlock } from "@/components";
+
 import styles from "./styles.module.css";
 
 export default function AccordionPage() {
   return (
-    <main className="prose h-screen flex flex-col item-center p-20">
+    <main className="prose mx-auto max-w-[1080px] h-screen flex flex-col item-center p-20">
       <h1>Accordion</h1>
-      <section>gkgk</section>
+      <div className={styles.accordion}>
+        <div>this is Accordion</div>
+      </div>
+      <CodeBlock code={code} />
     </main>
   );
 }
+
+const code = `
+async function sayHello(name) {
+  console.log('Hello', name);
+}
+`;
